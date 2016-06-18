0cacbc64483a75c49f87a16a74293149cc9ac177
lishu <1830077485@qq.com>
2016-06-19 01:27:55 +0800

    ghpages

---
+++ js/main.js
@@ -0,0 +1,10 @@
+/**
+ * Created by ls on 2016/3/8.
+ */
+$(function(){
+    var documwidth=$( window ).width();
+    $('.p-ct').css('width',documwidth+'px');
+    console.log(documwidth);
+    play();
+
+})
